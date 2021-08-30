df64d1965bc971113f77fe2ecf6104b0185505a1
douguizilla <douglasgommez@gmail.com>
2021-08-30 17:57:13 -0300

    implemented sendImplicitBroadcast

--- app/src/main/java/com/odougle/broadcastreceiver/MainActivity.kt
+++ app/src/main/java/com/odougle/broadcastreceiver/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.odougle.broadcastreceiver
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.odougle.broadcastreceiver.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding : ActivityMainBinding by lazy{
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.btnSend.setOnClickListener{
+            sendImplicitBroadcast()
+        }
+    }
+
+    private fun sendImplicitBroadcast() {
+        val intent = Intent(ACTION_EVENT)
+        sendBroadcast(intent)
+    }
+
+    companion object{
+        private const val ACTION_EVENT = "com.odougle.broadcastreceiver.ACTION_EVENT"
     }
 }
