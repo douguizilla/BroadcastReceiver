cde3157843e99d0fc09d7c577123b7feaba24d76
douguizilla <douglasgommez@gmail.com>
2021-08-30 18:07:32 -0300

    implemented local broadcast

--- app/src/main/java/com/odougle/broadcastreceiver/MainActivity.kt
+++ app/src/main/java/com/odougle/broadcastreceiver/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.odougle.broadcastreceiver
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.odougle.broadcastreceiver.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val binding : ActivityMainBinding by lazy{
         ActivityMainBinding.inflate(layoutInflater)
     }
+
+    val receiver: InternalReceiver = InternalReceiver()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +23,22 @@ class MainActivity : AppCompatActivity() {
         binding.btnSend.setOnClickListener{
             sendImplicitBroadcast()
         }
+
+        binding.btnLocal.setOnClickListener{
+            val intent = Intent(ACTION_EVENT)
+            LocalBroadcastManager.getInstance(this ).sendBroadcast(intent)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val filterLocal = IntentFilter(ACTION_EVENT)
+        LocalBroadcastManager.getInstance(this).registerReceiver(receiver, filterLocal)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(receiver)
     }
 
     private fun sendImplicitBroadcast() {
@@ -24,7 +46,13 @@ class MainActivity : AppCompatActivity() {
         sendBroadcast(intent)
     }
 
-    companion object{
+    inner class InternalReceiver : BroadcastReceiver(){
+        override fun onReceive(context: Context?, intent: Intent) {
+            binding.txtMessage.text = "Ação\n${intent.action}"
+        }
+    }
+
+    companion object {
         private const val ACTION_EVENT = "com.odougle.broadcastreceiver.ACTION_EVENT"
     }
 }
