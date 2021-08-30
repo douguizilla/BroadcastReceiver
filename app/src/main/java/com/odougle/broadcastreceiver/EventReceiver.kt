850badd4552c6dd7be3cf52ad544b02629196ed7
douguizilla <douglasgommez@gmail.com>
2021-08-30 17:49:09 -0300

    created EventReceiver.kt and implemented its method onReceiver

---
+++ app/src/main/java/com/odougle/broadcastreceiver/EventReceiver.kt
@@ -0,0 +1,14 @@
+package com.odougle.broadcastreceiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class EventReceiver : BroadcastReceiver(){
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val action = intent?.action
+        Toast.makeText(context, "Ação:\n$action", Toast.LENGTH_LONG).show()
+    }
+
+}
